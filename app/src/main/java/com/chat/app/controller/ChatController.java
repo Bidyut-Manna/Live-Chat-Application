4f8b10b8558390bce39c3c687377b9523a241497
Bidyut-Manna <mannabidyut2302@gmail.com>
2026-04-11 12:47:27 +0530

    Initial commit

---
+++ app/src/main/java/com/chat/app/controller/ChatController.java
@@ -0,0 +1,26 @@
+package com.chat.app.controller;
+
+import com.chat.app.model.ChatMessage;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ChatController {
+
+    //Whoever wants to send message in this /app/sendMessage then everyone who is subscribed to
+    // the /topic/message would get the message
+    @MessageMapping("/sendMessage")  //maps web socket messages to the destination
+    @SendTo("/topic/messages")
+    public ChatMessage sendMessage(ChatMessage message){
+        return message;
+    }
+
+    @GetMapping("/chat")
+    public String chat(){
+        return "chat";
+    }
+
+}
